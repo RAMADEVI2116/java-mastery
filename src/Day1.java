66e10b764715241eefa5ae4d787723d07184d309
Ramadevi <palaramadevi2116@gmail.com>
2026-02-27 11:30:24 +0530

    Day 1 - Java basics practice completed

---
+++ src/Day1.java
@@ -0,0 +1,15 @@
+class Day1 {
+    public static void main(String a[]){
+        int age = 25;
+        double salary = 25000.50;
+        char grade = 'A';
+        boolean isWorking = true;
+        String name = "ram";
+
+        System.out.println("Name: " +name);
+        System.out.println("Age: " +age);
+        System.out.println("Salary: " +salary);
+        System.out.println("Grade: " +grade);
+        System.out.println("Working: " +isWorking);
+    }
+}
