0f18a4381b3b757af337cad7cf0d4f7967d829fd
Ramadevi <palaramadevi2116@gmail.com>
2026-02-27 11:47:35 +0530

    Day 1 - Java basics practice completed

---
+++ src/Day1Arithmetic.java
@@ -0,0 +1,12 @@
+public class Day1Arithmetic {
+    public static void main(String args[]){
+        int a = 7;
+        int b = 3;
+
+        System.out.println("addition : " + (a+b) );
+        System.out.println("subtraction : " + (a-b) );
+        System.out.println("multiplication : " + (a*b) );
+        System.out.println("division : " + (a/b) );
+        System.out.println("modulus : " + (a%b) );
+    }
+}
---
+++ src/Day1TypeCast.java
@@ -0,0 +1,13 @@
+public class Day1TypeCast {
+    public static void main(String args[]){
+        int a = 10;
+        double x = (double)a;
+
+        double b = 9.8;
+        int y = (int) b;
+
+        System.out.println(x);
+        System.out.println(y);
+
+    }
+}
