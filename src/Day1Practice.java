4ad6f92f9fab5ee9fa0e9034338c397ee978d878
Ramadevi <palaramadevi2116@gmail.com>
2026-02-28 02:33:50 +0530

    Day 1 - Java basics practice completed

---
+++ src/Day1Practice.java
@@ -0,0 +1,49 @@
+public class Day1Practice {
+    //variable
+    //arithmetic
+    //type casting
+    public static void main(String[] args){
+        /*variable
+        int a = 10;
+        double b = 23.89356;
+        float c = 23.445f;
+        char d = 's';
+        boolean e = true;
+        byte f = 126;
+        short g = 3;
+        long h = 268733;
+        System.out.println("all values : "+a+" "+b+" "+c+" "+d+" "+e+" "+f+" "+g+" "+h);
+        */
+        /*arithmetic
+        int a = 7;
+        int b = 3;
+        System.out.println("Addition : "+ (a+b) );
+        System.out.println("Subtraction : "+ (a-b) );
+        System.out.println("Multiplication : "+ (a*b) );
+        System.out.println("division : "+ (a/b) );
+        System.out.println("modulus : "+ (a%b) );
+         */
+        //Type casting
+        //int a = 12;
+        //byte b = 126;
+        //a = b;
+        //System.out.println(a);
+        //int a = 257;
+        //byte b = 12;
+        //b = (byte)a;
+        //System.out.println(b);
+        //double a = 23.43687;
+        //float b = 23.45f;
+        //a = b;
+        //System.out.println(a);
+        //double a = 23.7857686845676;
+        //float b = 23.78f;
+        //b = (float)a;
+        //System.out.println(b);
+        //char c = 'd';
+        //int a = 64;
+        //a = c;
+        //c = (char)a;
+        //System.out.println(a+" "+c);
+    }
+}
